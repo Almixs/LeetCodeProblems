1b5465ceff39ac5354efb4e51a177bbcdfa2a802
Danylo <114859769+Almixs@users.noreply.github.com>
2025-02-19 00:03:48 +0200

    Update Roman_to_Integer-EASY.py

--- StringProblems/Roman_to_Integer-EASY.py
+++ StringProblems/Roman_to_Integer-EASY.py
@@ -56,5 +56,20 @@ def romanToInt(self, s: str) -> int:
 
 # submitted at Feb 17, 2025 14:23 // Beats 88.08%
 
+def roman(num):
+    roman_numerals = [
+        (1, "I"), (4, "IV"), (5, "V"), (9, "IX"), (10, "X"), (40, "XL"), (50, "L"), (90, "XC"), (100, "C"), (400, "CD"),
+        (500, "D"), (900, "CM"), (1000, "M")
+    ]
+    result = ""
+
+    for value, symbol in reversed(roman_numerals):
+        while num >= value:
+            result += symbol
+            num -= value
+
+    return result
+
+
 
 
